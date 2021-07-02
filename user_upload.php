80a490f0857f240aa700dd52321b42fa2e3ed5b7
Lavina Lobo <reachlavina@gmail.com>
2021-07-02 22:26:31 +1000

    Generate the insert concatenated insert query

--- user_upload.php
+++ user_upload.php
@@ -8,9 +8,23 @@ if (($h = fopen("users.csv", "r")) !== FALSE)
   // Convert each line into the local $data variable
   while (($data = fgetcsv($h, 1000, ",")) !== FALSE) 
   {
-      //print_r($data);
-   		     
-}
+      
+   if ($cntr >=1 ){
+     //print_r($data);
+     $firstname = ucfirst($data[0]); 
+     $lastname =  ucfirst($data[1]); 
+     $email = $data[2];
+     $sql .= "INSERT INTO users (firstname, surname, email) VALUES ('John', 'Doe', 'john@example.com');";
+
+      
+   }
+     $cntr++;
+  }
+  echo $sql;
+  // Close the file
+  fclose($h);
+
+
 }
 
 ?>
