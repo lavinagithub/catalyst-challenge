25516e144e3d0d2abf75b85528dce743b35aeffe
Lavina Lobo <reachlavina@gmail.com>
2021-07-03 12:44:41 +1000

    function for valid email, generate sql string

--- user_upload.php
+++ user_upload.php
@@ -1,5 +1,14 @@
 <?php
+// file includes
 include("./gifnoc.php");
+
+// function to check valid email address
+function checkValidEmail($email){
+ if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+       return false;
+      }
+}
+
 // Open the file for reading
 if (($h = fopen("users.csv", "r")) !== FALSE) 
 {
@@ -7,34 +16,36 @@ if (($h = fopen("users.csv", "r")) !== FALSE)
   // Convert each line into the local $data variable
   $sql_head = " INSERT INTO users (firstname, surname, email) VALUES ";
   $sql_val = "";
+  $emailError = "";
   while (($data = fgetcsv($h, 1000, ",")) !== FALSE) 
   {
    if ($cntr >=1 ){
-     echo $cntr;
-     print_r($data);  
-     $firstname = ucfirst(strtolower(trim($data[0]))); 
-     $lastname =  ucfirst(strtolower(trim($data[1]))); 
+     //echo $cntr;
+     //print_r($data);  
      $email = strtolower(trim($data[2]));
-     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        //$emailErr = "Invalid email format"; 
-       $email = "invalid".$cntr;
-      }
-     $sql_val .= "('".addslashes($firstname)."', '".addslashes($lastname)."', '".addslashes($email)."'),";    
+     if(checkValidEmail($email) !== false){
+       echo "here";
+       $firstname = ucfirst(strtolower(trim($data[0]))); 
+       $lastname =  ucfirst(strtolower(trim($data[1]))); 
+       $sql_val .= "('".addslashes($firstname)."', '".addslashes($lastname)."', '".addslashes($email)."'),";   
+     }else{
+       $emailError .= " $cntr - $email";
+     }
    }
      $cntr++;
   }
-  echo $sql_head . $sql_val;
-  
-    // if ($conn->query($sql) === TRUE) {
-    //   echo "New record created successfully";
-    //   } else {
-    //     echo "Error: " . $sql . "<br>" . $conn->error;
-    //   }
+  $sqlstring = $sql_head . $sql_val;
+
+  $sqlstring = rtrim($sqlstring, ",");
+
+    if ($conn->query($sql) === TRUE) {
+      echo "New record created successfully";
+      } else {
+        echo "Error: " . $sqlstring . "<br>" . $conn->error;
+      }
 
   // Close the file
   fclose($h);
-
-
 }
 
 ?>
